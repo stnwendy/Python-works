62547d4e154ce861dab0989534c4bdca99a53d15
stnwendy <96838362+stnwendy@users.noreply.github.com>
2024-05-18 03:40:06 +0800

    Update 05. Digit Finder.py

--- small-acts/05. Digit Finder.py
+++ small-acts/05. Digit Finder.py
@@ -1,15 +1,18 @@
 #input
-pwr=eval(input("Kindly input your desired power: "))
-dgt=eval(input("How many digits do you want to find in letter C? "))
+pwr=eval(input("Kindly input your desired power in 2^n: "))
+dgt=eval(input("What is the maximum number of last digits do you want to find? "))
   
 #define function  
 def print_dgt_fndr():
+    val = (pow(2,pwr))
     dgt_fndrA= (pow(2,pwr)) % 10
     dgt_fndrB= (pow(2,pwr)) % 100
     dgt_fndrC= (pow(2,pwr)) % (pow(10,dgt))
+    
     print("A.)The last digit when 2 is raised to ", pwr, " is ", dgt_fndrA, sep="")
     print("B.)The last two digits when 2 is raised to ", pwr, " is ", dgt_fndrB, sep="")
     print("C.)The last ", dgt," digits when 2 is raised to ", pwr, " is ", dgt_fndrC, sep="")
+    print("The value of 2^",pwr,"is ",val)
     
 #call
 print_dgt_fndr()
