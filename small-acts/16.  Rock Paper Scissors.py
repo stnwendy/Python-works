2d324e8239bbc4f4e5ee6aa5d038567336cb6c3c
stnwendy <96838362+stnwendy@users.noreply.github.com>
2024-05-18 05:11:57 +0800

    Update 16.  Rock Paper Scissors.py

--- small-acts/16.  Rock Paper Scissors.py
+++ small-acts/16.  Rock Paper Scissors.py
@@ -2,38 +2,26 @@ from random import choice
 
 game = ["ROCK", "PAPER", "SCISSORS"]
 
-round = eval(input("How many rounds do you want to play? "))
+rounds = int(input("How many rounds do you want to play? "))
+win = 0  
 
-for i in range (round):
+for _ in range(rounds):  
     computer_player = choice(game)
     player_input = input("Type your choice between ROCK, PAPER, or SCISSORS: (use only capital letters) ")
     print(f"Computer chose {computer_player}.")
     
-    win = 0
     if computer_player == player_input:
         print("Draw.")
         
-    elif computer_player == "ROCK" and player_input == "SCISSORS":
-        print("Computer wins.")
-
-    elif computer_player == "PAPER" and player_input == "ROCK":
-        print("Computer wins.")
-        
-    elif computer_player == "SCISSORS" and player_input == "PAPER":
+    elif (computer_player == "ROCK" and player_input == "SCISSORS") or \
+         (computer_player == "PAPER" and player_input == "ROCK") or \
+         (computer_player == "SCISSORS" and player_input == "PAPER"):
         print("Computer wins.")
         
-    elif player_input == "ROCK" and computer_player == "SCISSORS":
+    elif (player_input == "ROCK" and computer_player == "SCISSORS") or \
+         (player_input == "PAPER" and computer_player == "ROCK") or \
+         (player_input == "SCISSORS" and computer_player == "PAPER"):
         print("Player wins.")
-        win += 1
+        win += 1  
 
-    elif player_input == "PAPER" and computer_player == "ROCK":
-        print("Player wins.")
-        win += 1
-        
-    elif player_input == "SCISSORS" and computer_player == "PAPER":
-        print("Player wins.")
-        win += 1
-        
-
-print(f"\nYou won {win}/{round} matches.")
-
+print(f"\nYou won {win}/{rounds} matches.")
